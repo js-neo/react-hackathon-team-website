38a7cc4cdbcdd3cf997b1adaac7a19dacdcdbb95
js-neo <yuriy.jsx@gmail.com>
2024-09-12 23:48:29 +0300

    fix: change App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
+import {Route, Switch, Redirect, useLocation} from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Breadcrumbs from './components/breadcrumbs/breadcrumbs';
 import Footer from './components/footer/footer';
@@ -11,31 +11,26 @@ import Member from './pages/member';
 import PageNotFound from './pages/404';
 
 const App = () => {
-  const location = useLocation();
+    const {pathname} = useLocation();
+    const excludedPaths = ["/", "/404"];
 
-  return (
-    <>
-      <Navbar />
-      {location.pathname !== '/' && location.pathname !== '/404' ? (
-        <Breadcrumbs />
-      ) : null}
-      <Switch>
-        <Route path="/" exact>
-          <Home/>
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-        <Route path="/about" component={About} />
-        <Route path="/contacts" component={Contacts} />
-        <Route path="/member/:memberId" component={Member} />
-        <Route path="/404" component={PageNotFound} />
-        <Redirect from="/react-hackathon-team-website/" to="/" />
-        <Redirect to="/404" />
-      </Switch>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            {!excludedPaths.includes(pathname) && <Breadcrumbs/>}
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/favorites" component={Favorites}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contacts" component={Contacts}/>
+                <Route path="/member/:memberId" component={Member}/>
+                <Route path="/404" component={PageNotFound}/>
+                <Redirect from="/react-hackathon-team-website/" to="/"/>
+                <Redirect to="/404"/>
+            </Switch>
+            <Footer/>
+        </>
+    );
 };
 
 export default App;
